f421e00e0530960f461e0a7fd009a88bd125b5eb
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-27 01:48:58 -0700

    generate family buttons

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () =>{
+//selectors and listeners
+const peopleSelectors = document.querySelector('#peopleSelectors')
 
+
+//fetch
+//fetch family from db
+function fetchFamily(){
+    fetch("http://localhost:3000/jeffriesFamily")
+    .then(r => r.json())
+    .then(displayFamily)
+}
+
+function displayFamily(members){
+    members.forEach(members => renderFamily(members))
+}
+function renderFamily(member){
+    const memberButton = document.createElement('span')
+    memberButton.classList = "boxButton"
+    memberButton.id = member.id
+    memberButton.innerHTML = member.name
+    peopleSelectors.appendChild(memberButton)
+    
+}
+
+
+
+
+
+fetchFamily()
+})
