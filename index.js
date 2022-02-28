fd38dded79ba19d446e5e1a9f73b46458c0d36ae
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-28 00:45:47 -0700

    populating table

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () =>{
 //selectors and listeners
 const peopleSelectors = document.querySelector('#peopleSelectors')
-
-
+const openRidetitle = document.querySelector('#openRideTitle')
+const closedRidetitle = document.querySelector('#closedRideTitle')
+let addOpenRides = true
+let addClosedRides = true
+const closedRideTable=document.querySelector('#closedRides');
+const openRideTable=document.querySelector('#openRides');
+const allParks=document.querySelector('#allParks');
+const magicKingdom=document.querySelector('#magicKingdom');
+const epcot=document.querySelector('#epcot');
+const hollywoodStudios=document.querySelector('#hollywoodStudios');
+const animalKingdom=document.querySelector('#animalKingdom');
 //fetch
 //fetch family from db
 function fetchFamily(){
@@ -11,7 +20,29 @@ function fetchFamily(){
     .then(displayFamily)
 }
 function fetchAnimalKingdom(){
+    fetch("https://queue-times.com/en-US/parks/8/queue_times.json",)
+    .then(r => r.json())
+    .then(function (r){
+        cleanUpFetch(r)
+        })
+}
+function fetchMagicKingdom(){
+    fetch("https://queue-times.com/en-US/parks/6/queue_times.json",)
+    .then(r => r.json())
+    .then(function (r){
+        cleanUpFetch(r)
+        })
+}
+
+function fetchHollywoodStudios(){
     fetch("https://queue-times.com/en-US/parks/7/queue_times.json",)
+    .then(r => r.json())
+    .then(function (r){
+        cleanUpFetch(r)
+        })
+}
+function fetchEpcot(){
+    fetch("https://queue-times.com/en-US/parks/5/queue_times.json",)
     .then(r => r.json())
     .then(function (r){
         cleanUpFetch(r)
@@ -22,7 +53,17 @@ function fetchAnimalKingdom(){
     .then(r => r.json())
     .then(heightElement)
 }
+//event listeners
+allParks.addEventListener("click", fetchAnimalKingdom)
+allParks.addEventListener("click", fetchMagicKingdom)
+allParks.addEventListener("click", fetchHollywoodStudios)
+allParks.addEventListener("click", fetchEpcot)
+magicKingdom.addEventListener("click", fetchMagicKingdom)
+hollywoodStudios.addEventListener("click", fetchHollywoodStudios)
+epcot.addEventListener("click", fetchEpcot)
+animalKingdom.addEventListener("click", fetchAnimalKingdom)
 
+//functions
 function displayFamily(members){
     members.forEach(members => renderFamily(members))
 }
@@ -40,7 +81,7 @@ function renderFamily(member){
 function cleanUpFetch(lands){
     let landProp = lands.lands
       for(i=0; i<landProp.length; i++){
-        landProp[i].rides.forEach(ride=>addHeightElement(ride))
+        landProp[i].rides.forEach(ride=>checkOpen(ride))
       }  
 }
 
@@ -48,9 +89,9 @@ function checkOpen(ride){
     //confirmed this works, keep here
     console.log(ride.is_open)
     if(ride.is_open === true){
-       console.log(ride,"is open")
+       renderOpenRideTable(ride)
     }
-    else  console.log(ride, "is closed")
+    else renderClosedRideTable(ride)
     
 }
 
@@ -62,21 +103,69 @@ function heightElement(heightData){
     globalThis.over44=heightData[1].over44.concat(over40)
     globalThis.over48=heightData[0].over48.concat(over44)   
     }
-function addHeightElement(ride){
-    for(i=0;i<over32.length;i++){
-        console.log(over32[i])
-        if(over32[i]=ride.name){
-            console.log(ride)
-        }
+
+function renderOpenRideTable(ride){
+    if (typeof ride != "undefined" && addOpenRides ){
+        openRidetitle.textContent = "These Rides are Open"
+        let row= document.createElement('tr')
+        let heading1= document.createElement('th')
+        heading1.innerHTML= "Ride Name"
+        let heading2= document.createElement('th')
+        heading2.innerHTML= "Wait time"
+        row.appendChild(heading1)
+        row.appendChild(heading2)
+        openRideTable.appendChild(row)
+        addOpenRides=false;
+       
+        renderOpenRides(ride)
+       }
+       else renderOpenRides(ride)
+
+}
+function renderOpenRides(ride){
+    let row= document.createElement('tr')
+    let cell1= document.createElement('td')
+    cell1.innerHTML= `${ride.name}`
+    let cell2= document.createElement('td')
+    cell2.innerHTML= `${ride.wait_time}`
+    row.appendChild(cell1)
+    row.appendChild(cell2)
+    openRideTable.appendChild(row)
+
+}
+function renderClosedRideTable(ride){
+    if (typeof ride != "undefined" && addClosedRides ){
+    closedRidetitle.textContent = "These Rides are Closed"
+    let row= document.createElement('tr')
+    let heading1= document.createElement('th')
+    heading1.innerHTML= "Ride Name"
+    let heading2= document.createElement('th')
+    heading2.innerHTML= "Wait time"
+    row.appendChild(heading1)
+    row.appendChild(heading2)
+    closedRideTable.appendChild(row)
+    addClosedRides=false;
+   
+    renderClosedRides(ride)
+   }
+   else renderClosedRides(ride)
+    
+}
+function renderClosedRides(ride){
+    let row= document.createElement('tr')
+    let cell1= document.createElement('td')
+    cell1.innerHTML= `${ride.name}`
+    let cell2= document.createElement('td')
+    cell2.innerHTML= `${ride.wait_time}`
+    row.appendChild(cell1)
+    row.appendChild(cell2)
+    closedRideTable.appendChild(row)
+   
     }
-}
+    
 
 
-function renderRides(ride){
-    console.log(ride)
-}
 
-fetchAnimalKingdom()
 
 fetchFamily()
 fetchHeightInfo()
